2274eca74add5a6bc6b1a934728a82b856e82c7d
ozitraveller <ozitraveller@gmail.com>
2020-06-15 17:04:02 +1000

    3.0.0

---
+++ ob-64/config/includes.chroot/etc/skel/vbox-additions.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+#~ 1. sudo apt-get install linux-headers-amd64 linux-headers-4.9.0-9-amd64 build-essential
+#~ 2. reboot
+#~ 3. sudo apt-get install dkms
+#~ 4. cd /media/cdrom0
+#~ 5. sudo sh ./VBoxLinuxAdditions.run
+
+# Manual mount and install
+# http://blog.oracle48.nl/installing-virtualbox-guest-additions-using-the-command-line/
+#-----------------------------
+#~ 2. Mount the DVD drive on Guest OS
+#~ [guest] # mkdir /mnt/dvd
+#~ [guest] # mount -t iso9660 -o ro /dev/dvd /mnt/dvd
+#~ 3. Install VBoxLinuxAdditions
+#~ [guest] # cd /mnt/dvd
+#~ [guest] # ./VBoxLinuxAdditions.run
+#~ If you encounter errors here, see: ‘0. Common Errors‘.
+
+#~ 4. Done? Unmount and remove
+#~ [guest] # umount /dev/dvd
+
+
+NOTE: 
+apt-get install linux-headers-amd64 linux-headers-4.9.0-9-amd64 build-essential
+# reboot
+sudo apt-get install dkms
+
+
+
+mkdir ~/host
+sudo su
+
+mkdir /mnt/dvd
+# insert guest-additions cd
+mount -t iso9660 -o ro /dev/dvd /mnt/dvd
+cd /mnt/dvd
+./VBoxLinuxAdditions.run
+cd ../..
+umount /mnt/dvd
+
+
+# alias mshr='sudo mount -t vboxsf -o uid=$UID,gid=$(id -g) share ~/host'
+source ~/.bashrc
+
+exit 0
